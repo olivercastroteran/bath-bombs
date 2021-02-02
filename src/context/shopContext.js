b8da0a2eebe18ad917de445b2ae36f54ee440d49
oliver <olivercastroteran@gmail.com>
2021-02-01 22:35:56 -0400

    shopify partners account created and link plus basic shopcontex

--- src/context/shopContext.js
+++ src/context/shopContext.js
@@ -1,15 +1,50 @@
 import React, { Component } from 'react';
 import Client from 'shopify-buy';
 
+const ShopContext = React.createContext();
+
 const client = Client.buildClient({
-  domain: 'your-shop-name.myshopify.com',
-  storefrontAccessToken: 'your-storefront-access-token',
+  domain: process.env.REACT_APP_SHOPIFY_DOMAIN,
+  storefrontAccessToken: process.env.REACT_APP_SHOPIFY_API,
 });
 
 class ShopProvider extends Component {
+  state = {
+    product: {},
+    products: [],
+    checkout: {},
+    isCartOpen: false,
+    isMenuOpen: false,
+  };
+
+  createCheckout = async () => {};
+
+  fetchCheckout = async () => {};
+
+  addItemToCheckout = async () => {};
+
+  removeLineItem = async (lineItemsIdsToRemove) => {};
+
+  fetchAllProducts = async () => {};
+
+  fetchProductWithHandle = async (handle) => {};
+
+  closeCart = () => {};
+
+  openCart = () => {};
+
+  closeMenu = () => {};
+
+  openMenu = () => {};
+
   render() {
-    return <div></div>;
+    console.log(client);
+    return <ShopContext.Provider>{this.props.children}</ShopContext.Provider>;
   }
 }
+
+const ShopConsumer = ShopContext.Consumer;
+
+export { ShopConsumer, ShopContext };
 
 export default ShopProvider;
